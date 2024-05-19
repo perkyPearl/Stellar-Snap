771b3df22c817472c859295d893b8005eee9b3ce
Pearl <pearl.dhiman2004@gmail.com>
2024-05-19 17:55:23 +0530

    Login Expression Error Fixed

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,7 +6,7 @@ const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-4
+
   const handleLogin = (e) => {
     e.preventDefault();
 
